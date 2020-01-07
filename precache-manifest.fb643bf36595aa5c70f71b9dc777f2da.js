f3f5d8d039f0d277d0a41fac6c9b60005b570411
poltergeist <spectator.networx@gmail.com>
2020-01-07 21:00:15 +0000

    Updates

---
+++ precache-manifest.fb643bf36595aa5c70f71b9dc777f2da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfabb6f7de4178038011820126010c78",
+    "url": "/dampfgaren/index.html"
+  },
+  {
+    "revision": "ba2128f26e921acde76c",
+    "url": "/dampfgaren/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "da737e3571fea86f0700",
+    "url": "/dampfgaren/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba2128f26e921acde76c",
+    "url": "/dampfgaren/static/js/2.a6a5c64a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dampfgaren/static/js/2.a6a5c64a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da737e3571fea86f0700",
+    "url": "/dampfgaren/static/js/main.7a5b5fe3.chunk.js"
+  },
+  {
+    "revision": "27592049f71abfef0e65",
+    "url": "/dampfgaren/static/js/runtime-main.30d600a0.js"
+  }
+]);
